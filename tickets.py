c3ac8a0bd9240676edaae24582d93c377c99f124
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-06 10:36:54 -0400

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -4,9 +4,9 @@ tickets_remaining = 100
 
 print("There are {} tickets remaining.".format(tickets_remaining))
 
-name = input("What is your name")
+name = input("What is your name: ")
 
-num_tickets = input("How many tickets would you like, {}".format(name))
+num_tickets = input("How many tickets would you like, {}: ".format(name))
 num_tickets = int(num_tickets)
 
 amount_due = num_tickets * TICKET_PRICE
