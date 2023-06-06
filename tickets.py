9458dc3b324130dc3d4bb3c6e316ceef31f86919
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-06 10:48:35 -0400

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -1,14 +1,27 @@
+import sys
+
 TICKET_PRICE = 10
 
 tickets_remaining = 100
 
 print("There are {} tickets remaining.".format(tickets_remaining))
 
-name = input("What is your name: ")
+name = input("What is your name?: ")
 
-num_tickets = input("How many tickets would you like, {}: ".format(name))
+num_tickets = input("How many tickets would you like, {}?: ".format(name))
 num_tickets = int(num_tickets)
 
 amount_due = num_tickets * TICKET_PRICE
 
 print("Total due is ${}".format(amount_due))
+
+will_proceed = input("Do you want to proceed with the purchase?: Y/N  ")
+
+if will_proceed.upper() == "Y":
+    print("Sold!")
+    tickets_remaining -= num_tickets
+
+else: 
+    print("Thank you anyway, {}".format(name))
+
+sys.exit()
