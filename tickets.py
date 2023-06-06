274f85bc5dcede68f2c2b82db6458eb18b80acfb
Michael Plantamura <105463635+Michaelplant2@users.noreply.github.com>
2023-06-06 10:55:06 -0400

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -7,22 +7,16 @@ tickets_remaining = 100
 while tickets_remaining:
 
     print("There are {} tickets remaining.".format(tickets_remaining))
-
     name = input("What is your name?: ")
-
     num_tickets = input("How many tickets would you like, {}?: ".format(name))
     num_tickets = int(num_tickets)
-
     amount_due = num_tickets * TICKET_PRICE
-
     print("Total due is ${}".format(amount_due))
-
     will_proceed = input("Do you want to proceed with the purchase?: Y/N  ")
 
     if will_proceed.upper() == "Y":
         print("Sold!")
         tickets_remaining -= num_tickets
-
     else: 
         print("Thank you anyway, {}".format(name))
 
